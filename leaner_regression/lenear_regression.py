9b16e3e2e7d23e6bf9254c8d8c326debd7d83824
Batsal Awaley <batsal@vayu.com.au>
2017-10-20 20:52:42 +0545

    testing assumptions linear regression

--- leaner_regression/lenear_regression.py
+++ leaner_regression/lenear_regression.py
@@ -4,11 +4,28 @@ from statistics import mean
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import style
+import random
 
 style.use('fivethirtyeight')
 
 xs = np.array([1, 2, 3, 4, 5, 6], dtype=np.float64)
 ys = np.array([5, 4, 6, 5, 6, 7], dtype=np.float64)
+
+# for testing
+def create_dataset(hm, variance, step=2, correlation=False):
+    val = 1
+    ys = []
+    for i in range(hm):
+        y = val + random.randrange(-variance, variance)
+        ys.append(y)
+        if correlation and correlation == 'pos':
+            val+=step
+        elif correlation and correlation == 'neg':
+            val -= step
+
+    xs = [i for i in range(len(ys))]
+
+    return np.array(xs, dtype=np.float64), np.array(ys, dtype=np.float64)
 
 def best_fit_slope_and_intercept(xs,ys):
     m = (((mean(xs) * mean(ys)) - mean(xs * ys)) /
@@ -25,6 +42,7 @@ def coefficient_of_determination(ys_orig, ys_line):
     squared_error_y_mean = squared_error(ys_orig, y_mean_line)
     return 1 - (squared_error_regr / squared_error_y_mean)
 
+xs, ys = create_dataset(40, 10, 2, correlation='pos')
 
 m, b = best_fit_slope_and_intercept(xs, ys)
 
