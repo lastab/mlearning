310ebc754aa3319bb57fd7619734cd8c50b51b73
Batsal Awaley <batsal@vayu.com.au>
2017-10-20 06:13:08 +0545

    Forcast using regression

--- machine_learning_tutorial_regression.py
+++ machine_learning_tutorial_regression.py
@@ -1,8 +1,12 @@
 import pandas as pd
-import quandl, math
+import quandl, math, datetime, time
 import numpy as np
 from sklearn import preprocessing, cross_validation, svm
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+style.use('ggplot')
 
 quandl.ApiConfig.api_key = "T15PRmq9pXKvDatVsVvw"
 df = quandl.get("WIKI/AAPL")# quandl.get('WIKI/GOOGLE')
@@ -19,15 +23,14 @@ forcast_out = int(math.ceil(0.01*len(df)))
 print(forcast_out)
 
 df['label'] = df[forcast_col].shift(-forcast_out)
-df.dropna(inplace=True)
 
 X = np.array(df.drop(['label'], 1))
-y = np.array(df['label'])
-
 X = preprocessing.scale(X)
+X_lately = X[-forcast_out:]
+X = X[:-forcast_out]
 
-# X = X[:-forcast_out +1]
-# df.dropna(inplace=True)
+df.dropna(inplace=True)
+y = np.array(df['label'])
 y = np.array(df['label'])
 
 X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
@@ -40,4 +43,24 @@ clf.score(X_test, y_test)
 
 accuracy = clf.score(X_test, y_test)
 
-print(accuracy)
+forcast_set = clf.predict(X_lately)
+print(forcast_set, accuracy, forcast_out)
+
+df['Forcast'] = np.nan
+
+last_date = df.iloc[-1].name
+last_unix = time.mktime(last_date.timetuple())
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forcast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += one_day
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)] + [i]
+
+df['Adj. Close'].plot()
+df['Forcast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
